e719255f156760f071a6417cfe75dfd53ed228ff
blackcat5328 <167646747+blackcat5328@users.noreply.github.com>
2024-08-08 12:20:27 +0800

    Update matching_card_3d.js

--- src/matching_card_3d.js
+++ src/matching_card_3d.js
@@ -29,7 +29,7 @@ window.initGame = (React, assetsUrl) => {
     const chairUrl = `${assetsUrl}/chair.glb`; // Adjust the URL for chair model
     return React.createElement(CardModel, {
       url: chairUrl,
-      scale: [4, 4, 4],
+      scale: [4, 3, 4],
       position: position
     });
   }
